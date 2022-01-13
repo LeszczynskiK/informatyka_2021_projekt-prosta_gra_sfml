7a0b9ac72bb52d678e4f489830472e917629857c
LeszczynskiK <93399975+LeszczynskiK@users.noreply.github.com>
2022-01-13 13:40:09 +0000

    Edit prawie końcowy - dzien 13 styczen
    Zmiana niektorych tekstur w grze, dodanie drugiego menu wyboru dotyczacego stopnia trudnosci gry( wczesniej bylo poprzez cmd), lekka zmiana dynamiki w 2giej czesci gry

---
+++ Project2/Project2/zwierze.cpp
@@ -0,0 +1,34 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "deklaracje.h"
+using namespace std;
+using namespace sf;
+
+zwierze::zwierze(float x_in1, float y_in1)
+{
+	position1.x = x_in1;
+	position1.y = y_in1;
+	ZWIERZE.setPosition(position1);
+	ZWIERZE.setSize(sf::Vector2f(75, 75));
+	tekstura1.loadFromFile("dzik.png");
+	ZWIERZE.setTexture(&tekstura1);
+	tekstura1ty.loadFromFile("mysliwy.png");
+	ZWIERZEty.setPosition(position1);
+	ZWIERZEty.setSize(sf::Vector2f(75, 75));
+	ZWIERZEty.setTexture(&tekstura1ty);
+}
+
+void zwierze::przesun1(float x_in1, float y_in1)
+{
+	sf::Vector2f pos1;
+	pos1.x = x_in1;
+	pos1.y = y_in1;
+	ZWIERZE.move(pos1);
+}
+void zwierze::przesun1TY(float x_in1, float y_in1)
+{
+	sf::Vector2f pos1;
+	pos1.x = x_in1;
+	pos1.y = y_in1;
+	ZWIERZEty.move(pos1);
+}
